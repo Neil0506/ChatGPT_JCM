84a08a162dd4c19c3c0b252c577c01a99c367189
Neil0506 <91375914+Neil0506@users.noreply.github.com>
2023-03-25 01:31:13 +0800

    Update mutation-types.js

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,6 +1,6 @@
 //AI头像地址设置
-export const AI_HEAD_IMG_URL="https://imgloc.com/i/i4WbXw"
+export const AI_HEAD_IMG_URL="https://imgloc.com/i/i4Od0V"
 //用户头像地址设置
-export const USER_HEAD_IMG_URL="https://i.328888.xyz/2023/03/25/i4WbXw.png"
+export const USER_HEAD_IMG_URL="https://i.328888.xyz/2023/03/25/i4Od0V.png"
 //用户名称设置
 export const USER_NAME="scudpower"
