27c6b578ac0de83d27aa9b5ddf5aaed088d146f9
Neil0506 <91375914+Neil0506@users.noreply.github.com>
2023-03-25 00:52:45 +0800

    Update setting.vue

--- src/view/pages/setting.vue
+++ src/view/pages/setting.vue
@@ -12,8 +12,7 @@
         <transition name="el-zoom-in-top">
           <div v-show="show" class="transition-box">
             <span>
-              很感谢大家对我的支持，现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API后续会添加更多有意思的功能进去，希望大家给我的GitHub点个小小的星星，
-              大家如果有什么好的想法可以在GitHub中提出来，我们一起来完善这个页面。
+              基于开源项目修改，如有问题请联系小倪
             </span>
             <div>
               <a href="https://space.bilibili.com/326625155?spm_id_from=333.337.0.0">BliBili</a>---<a href="https://github.com/202252197/ChatGPT_JCM">GitHub</a>
